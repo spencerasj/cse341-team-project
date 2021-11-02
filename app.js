e29d6ad9e303666815cfa3424ab68bf4dddaa440
spencerasj <spe19021@byui.edu>
2021-11-01 20:53:06 -0600

    few extra things to app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const path = require("path");
 const express = require("express");
+require("dotenv").config();
 const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
 
 const app = express();
