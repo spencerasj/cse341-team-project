7b8c6629ac12693ac25fe9a32ad9a335ebb251fe
spencerasj <spe19021@byui.edu>
2021-11-01 20:46:20 -0600

    set up local host etc

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
+
+const app = express();
+app
+  .use(express.static(path.join(__dirname, "public")))
+  .set("view engine", "ejs")
+
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
