a609b100436dfddcd0065a028e098c5cbf2c9ba3
Jen Spencer <spe19021@byui.edu>
2021-11-02 23:12:34 -0600

    added mongodb connection

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const path = require("path");
 const express = require("express");
 require("dotenv").config();
+const mongoose = require("mongoose");
+
+const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
 
 const app = express();
+
 app
   .use(express.static(path.join(__dirname, "public")))
-  .set("view engine", "ejs")
+  .set("view engine", "ejs");
 
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
