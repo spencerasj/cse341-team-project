b4862ddf4176d5c090af8d5dff948695f3640a0e
John Brooke <bro16077@byui.edu>
2021-11-06 23:08:29 -0600

    work on group model, change start:dev script to dev

--- models/groups.js
+++ models/groups.js
@@ -3,7 +3,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const groupSchema = new Schema({
-    
+    teamName: {
+        type: String,
+        required: true
+    },
+    players: [
+        {
+            name: { type: String, required: true },
+            userId: {
+                type: Schema.Types.ObjectId,
+                required: true,
+                ref: 'User'
+            }
+        }
+    ]
 });
 
 module.exports = mongoose.model('Groups', groupSchema);
