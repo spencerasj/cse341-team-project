531cb882309f21b82376e6468a9e28788a889028
Jen Spencer <spe19021@byui.edu>
2021-11-08 12:38:35 -0700

    adding Heroku and cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
@@ -11,6 +12,19 @@ const app = express();
 app
   .use(express.static(path.join(__dirname, "public")))
   .set("view engine", "ejs");
+
+const corsOptions = {
+  origin: "https://git.heroku.com/beat-that.git",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  family: 4,
+};
 
 mongoose
   .connect(MONGODB_URI)
