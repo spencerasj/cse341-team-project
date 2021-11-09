cc838cc73307f76851a981639e54ff3d655a9f08
Mathieu Steele <matt@steeleagency.com>
2021-11-09 07:40:54 -0500

    add basic user schema

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  resetToken: String,
+  resetTokenExpiration: Date,
+});
+
+module.exports = mongoose.model("User", userSchema);
