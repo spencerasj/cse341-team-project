31becf26a1cbf4dca1162da1aadc5b33f2891fc4
Mathieu Steele <matt@steeleagency.com>
2021-11-09 07:39:58 -0500

    add placeholder auth middleware

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
-  if (!req.session.isLoggedIn) {
-    return res.redirect("/login");
-  }
+  // Uncomment this to require login
+  // if (!req.session.isLoggedIn) {
+  //   return res.redirect("/login");
+  // }
   next();
 };
