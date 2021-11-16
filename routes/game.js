0122b12a9f8c72e4cca13fd24a8da0ccf68e28ca
Mathieu Steele <matt@steeleagency.com>
2021-11-16 08:19:31 -0500

    add validation to game editing

--- routes/game.js
+++ routes/game.js
@@ -27,21 +27,17 @@ router.post(
 router.get("/edit/:gameId", isAuth, gameController.getEditGame);
 router.post(
   "/edit",
-  // validation middleware will go here. examples:
-  // [
-  //   body("title")
-  //     .isString()
-  //     .isLength({ min: 3 })
-  //     .trim()
-  //     .withMessage("the title must be at least 3 characters long"),
-  //   body("price")
-  //     .isFloat()
-  //     .withMessage("The price was not valid. Please try again."),
-  //   body("description")
-  //     .isLength({ min: 5, max: 400 })
-  //     .trim()
-  //     .withMessage("The description must be between 5 and 400 characters."),
-  // ],
+  [
+    body("name")
+      .isString()
+      .trim()
+      .isLength({ min: 3 })
+      .withMessage("the game name must be at least 3 characters long"),
+    body("description")
+      .trim()
+      .isLength({ min: 5, max: 400 })
+      .withMessage("The description must be between 5 and 400 characters."),
+  ],
   isAuth,
   gameController.postEditGame
 );
